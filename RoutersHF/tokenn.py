89cb440ed944d3601fd909b2c8d2d9dcd7c3b574
Hugo Flores <mralesshug@gmail.com>
2025-05-14 00:57:51 -0600

    reconocimiento de diccionario,faltante select de area y crear ticket

--- RoutersHF/tokenn.py
+++ RoutersHF/tokenn.py
@@ -3,15 +3,42 @@ import os
 import csv
 
 app = Flask(__name__, template_folder='../templates')
-DICCIONARIO_CSV = 'diccionario.csv'
 
-@app.route('/')
-def index():
+# Función para cargar problemas y soluciones desde el archivo CSV
+def cargar_diccionario():
+    diccionario = {}
+    with open('diccionario.csv', mode='r', encoding='utf-8') as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            problema = fila['Problema']
+            solucion = fila['Solucion']
+            if problema in diccionario:
+                diccionario[problema].append(solucion)
+            else:
+                diccionario[problema] = [solucion]
+    return diccionario
+
+diccionario = cargar_diccionario()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        soluciones = diccionario.get(descripcion, ["No se encontró solución para el problema."])
+        
+        resultado = f"<h5>Descripcion</h5> <li>{descripcion}</li> <h5>Solucion</h5> <ul>"
+        for solucion in soluciones:
+            resultado += f"<li>{solucion}</li>"
+        resultado += "</ul>"
+        
+        return redirect(url_for('mostrar_respuesta', resultado=resultado))
+    
     return render_template('crearticket.html')
 
-@app.route('/solucion')
-def solucion():
-    return render_template('solucion.html')
+@app.route('/mostrar_respuesta')
+def mostrar_respuesta():
+    resultado = request.args.get('resultado', '')
+    return render_template('solucion.html', resultado=resultado)
 
 @app.route('/seguimiento_ticket')
 def seguimiento_ticket():
